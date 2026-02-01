132b604a75cddcbfb0905f5451a6f38d8b4d40cf
pjheeta <pjheeta@gmail.com>
2026-01-31 20:28:28 -0800

    Chapter 1 - Lesson 7: Verbose Output

--- main.py
+++ main.py
@@ -19,15 +19,19 @@ def main():
     
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 
     response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
    
 
