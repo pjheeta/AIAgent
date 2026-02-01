0d976cd3f90045541dc0ad4a55d1ec008a73cc7c
pjheeta <pjheeta@gmail.com>
2026-01-31 19:19:13 -0800

    Chapter 1, Lesson 5: User Inputs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from google import genai
 from dotenv import load_dotenv
 
@@ -14,10 +15,12 @@ def main():
     if api_key == None:
         print("API key not found. Please set the GEMINI_API_KEY environment variable.") 
     
-    
-    response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )   
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    response = client.models.generate_content(model='gemini-2.5-flash', contents=args.user_prompt)
+
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
