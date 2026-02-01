e42d90af4e31dcf264b7fa6fe5dfa5034bf5dff8
pjheeta <pjheeta@gmail.com>
2026-01-31 19:13:07 -0800

    Prompt and Response Tokens

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ def main():
     response = client.models.generate_content(
     model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )   
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
    
 
