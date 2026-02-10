9b50293665bcacebde79357f13db95729e6bcfa9
pjheeta <pjheeta@gmail.com>
2026-02-09 20:04:38 -0800

    Chapter 4 - Lesson 1: Agent Loop

--- main.py
+++ main.py
@@ -33,45 +33,59 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
+    for _ in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
+            )
+        for apple in response.candidates:
+            messages.append(apple.content)
+        
+        if response.function_calls:
+            function_results = []  # Track results
+        
+            for function_call in response.function_calls:
+                # Call the function
+                function_call_result = call_function(function_call, verbose=args.verbose)
+                
+                # Verify we got a valid response
+                if not function_call_result.parts:
+                    raise Exception("Function call returned no parts")
+                
+                if function_call_result.parts[0].function_response is None:
+                    raise Exception("Function response is None")
+                
+                if function_call_result.parts[0].function_response.response is None:
+                    raise Exception("Function response.response is None")
+                
+                # Add to results
+                function_results.append(function_call_result.parts[0])
+                
+                # Print result if verbose
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+                # else:
+                #     print(f"No function calls, Here is the response in text {response.text}")
+  
+            messages.append(types.Content(role="user", parts=function_results))
+        
+        else:
+            print ("Final Response:")
+            print(response.text)
+            break
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
-        )
+        
+            
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
     print(response.text)
 
-    if response.function_calls:
-        function_results = []  # Track results
-        
-        for function_call in response.function_calls:
-            # Call the function
-            function_call_result = call_function(function_call, verbose=args.verbose)
-            
-            # Verify we got a valid response
-            if not function_call_result.parts:
-                raise Exception("Function call returned no parts")
-            
-            if function_call_result.parts[0].function_response is None:
-                raise Exception("Function response is None")
-            
-            if function_call_result.parts[0].function_response.response is None:
-                raise Exception("Function response.response is None")
-            
-            # Add to results
-            function_results.append(function_call_result.parts[0])
-            
-            # Print result if verbose
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(f"No function calls, Here is the response in text {response.text}")
+    
 
 
 
